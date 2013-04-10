bf275d1d4675796288ebfab1dbd590800610dc31
Dominik Bamberger <dmayer@suse.de>
2013-04-10 16:45:49 +0200

    prepare settings spec

---
+++ spec/base/settings_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Settings do
+
+  it 'reads options from a YML file'
+
+  it 'allows to save changes back to the file'
+
+  it 'offers convenient access to config options'
+
+end
