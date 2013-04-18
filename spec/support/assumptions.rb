e7fafc723abf4efe4482059799a092c555132bfd
b4mboo <bamboo@suse.com>
2013-04-18 12:54:08 +0200

    Remove obsolete assumption

--- spec/support/assumptions.rb
+++ spec/support/assumptions.rb
@@ -57,12 +57,6 @@ def assume_custom_target_branch_defined
   ENV.stub(:[]).with('TARGET_BRANCH').and_return(custom_target_name)
 end
 
-def assume_on_master_then_feature_branch
-  subject.stub(:git_call).with('branch').twice.and_return(
-    "* master\n", " master\n* #{branch_name}\n"
-  )
-end
-
 def assume_change_branches(direction = nil)
   if direction
     branches = ["* master\n  #{branch_name}\n", "  master\n* #{branch_name}\n"]
