ec85c8b747b8344ea930c9b72fa26ddb3e0201c2
xystushi <xystushi@gmail.com>
2013-04-20 00:10:37 +0800

    Use Github's API v3 to show comments for pull requests.

--- lib/base/internals.rb
+++ lib/base/internals.rb
@@ -162,38 +162,85 @@ module Internals
 
 
   # Show current discussion for @current_request.
+  # The structure of a discussion is like the following
+  # - Some pull request
+  #    - Comment 1
+  #    - Comment 2
+  #    - Some commit
+  #      -Comment 1 on commit
+  #      -Comment 2 on commit
+  #    - ...
   def discussion
-    # FIXME:
-    puts 'This needs to be updated to work with API v3.'
-    return
-    request = @github.pull_request source_repo, @current_request['number']
-    result = request['discussion'].collect do |entry|
-      user = entry['user'] || entry['author']
-      name = user['login'].empty? ? user['name'] : user['login']
-      output = "\e[35m#{name}\e[m "
-      case entry['type']
-        # Comments:
-        when "IssueComment", "CommitComment", "PullRequestReviewComment"
-          output << "added a comment"
-          output << " to \e[36m#{entry['commit_id'][0..6]}\e[m" if entry['commit_id']
-          output << " on #{format_time(entry['created_at'])}"
-          unless entry['created_at'] == entry['updated_at']
-            output << " (updated on #{format_time(entry['updated_at'])})"
-          end
-          output << ":\n#{''.rjust(output.length + 1, "-")}\n"
-          output << "> \e[32m#{entry['path']}:#{entry['position']}\e[m\n" if entry['path'] and entry['position']
-          output << entry['body']
-        # Commits:
-        when "Commit"
-          output << "authored commit \e[36m#{entry['id'][0..6]}\e[m on #{format_time(entry['authored_date'])}"
-          unless entry['authored_date'] == entry['committed_date']
-            output << " (committed on #{format_time(entry['committed_date'])})"
-          end
-          output << ":\n#{''.rjust(output.length + 1, "-")}\n#{entry["message"]}"
+    issue_comments = @github.issue_comments(source_repo, @current_request['number'])
+    pull_commits = @github.pull_commits(source_repo, @current_request['number'])
+    # A bit hacky here. Just put everything in chronological order.
+    # Issue comments and pull commits have different structures.
+    comments = (issue_comments + pull_commits).sort! { |x,y|
+      (x.created_at || x.commit.committer.date) <=> (y.created_at || y.commit.committer.date)
+    }
+    result = comments.collect do |entry|
+      output = ""
+      if entry.commit?
+        # it is a pull commit
+        name = entry.committer.login
+        output << "\e[35m#{name}\e[m "
+        output << "committed \e[36m#{entry['sha'][0..6]}\e[m on #{format_time(entry.commit.committer.date)}"
+        output << ":\n#{''.rjust(output.length + 1, "-")}\n#{entry.commit.message}"
+        # FIXME:
+        # Comments on commits does not work yet, as the commits may come from forks
+        # haven't found a reliable way to identify the forked repo.
+        # commit_comments = @github.commit_comments(source_repo, entry.sha)
+        # commit_comments.each do |cc|
+        # end
+      else
+        # it is a issue comment
+        name = entry.user.login
+        output << "\e[35m#{name}\e[m "
+        output << "added a comment"
+        output << " to \e[36m#{entry.id}\e[m"
+        output << " on #{format_time(entry.created_at)}"
+        unless entry['created_at'] == entry['updated_at']
+          output << " (updated on #{format_time(entry.updated_at)})"
+        end
+        output << ":\n#{''.rjust(output.length + 1, "-")}\n"
+        output << entry.body
       end
       output << "\n\n\n"
     end
     puts result.compact unless result.empty?
+
+
+    # # FIXME:
+    # puts 'This needs to be updated to work with API v3.'
+    # return
+    # request = @github.pull_request source_repo, @current_request['number']
+    # result = request['discussion'].collect do |entry|
+    #   user = entry['user'] || entry['author']
+    #   name = user['login'].empty? ? user['name'] : user['login']
+    #   output = "\e[35m#{name}\e[m "
+    #   case entry['type']
+    #     # Comments:
+    #     when "IssueComment", "CommitComment", "PullRequestReviewComment"
+    #       output << "added a comment"
+    #       output << " to \e[36m#{entry['commit_id'][0..6]}\e[m" if entry['commit_id']
+    #       output << " on #{format_time(entry['created_at'])}"
+    #       unless entry['created_at'] == entry['updated_at']
+    #         output << " (updated on #{format_time(entry['updated_at'])})"
+    #       end
+    #       output << ":\n#{''.rjust(output.length + 1, "-")}\n"
+    #       output << "> \e[32m#{entry['path']}:#{entry['position']}\e[m\n" if entry['path'] and entry['position']
+    #       output << entry['body']
+    #     # Commits:
+    #     when "Commit"
+    #       output << "authored commit \e[36m#{entry['id'][0..6]}\e[m on #{format_time(entry['authored_date'])}"
+    #       unless entry['authored_date'] == entry['committed_date']
+    #         output << " (committed on #{format_time(entry['committed_date'])})"
+    #       end
+    #       output << ":\n#{''.rjust(output.length + 1, "-")}\n#{entry["message"]}"
+    #   end
+    #   output << "\n\n\n"
+    # end
+    # puts result.compact unless result.empty?
   end
 
 
