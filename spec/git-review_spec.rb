f224a663ba0a01e09893db4504a1fbc1650c1523
xystushi <xystushi@gmail.com>
2013-06-23 13:15:47 +0800

    Refactor git-review_spec into contexts.

--- spec/git-review_spec.rb
+++ spec/git-review_spec.rb
@@ -4,29 +4,38 @@ require_relative '../lib/git-review'
 
 describe 'GitReview' do
 
-  context 'command' do
+  let(:cmd) { ::GitReview::Commands }
+  let(:gr) { ::GitReview::GitReview }
+  let(:gh) { ::GitReview::Github.instance }
 
-      let(:cmd) { ::GitReview::Commands }
-      let(:gr) { ::GitReview::GitReview }
-      let(:gh) { ::GitReview::Github.instance }
+  it 'shows help page if no arguments are given' do
+    cmd.should_receive(:help)
+    gr.new
+  end
 
-      it 'calls help when nil' do
-        cmd.should_receive(:help)
-        gr.new([nil])
-      end
+  it 'shows help page if argument is empty' do
+    cmd.should_receive(:help)
+    gr.new(%w())
+  end
 
-      it 'calls help when empty' do
-        cmd.should_receive(:help)
-        gr.new(%w())
-      end
+  it 'checks whether the command is valid' do
+    cmd.stub(:help)
+    cmd.should_receive(:respond_to?).with('foo')
+    gr.new(%w(foo))
+  end
 
-      it 'checks for validity' do
-        cmd.stub(:help)
-        cmd.should_receive(:respond_to?).with('foo')
-        gr.new(%w(foo))
-      end
+  context 'when command is valid' do
 
-    it 'calls command if valid' do
+    it 'calls update first unless command is clean' do
+      gr.any_instance.stub(:local_repo_ready).and_return(true)
+      gr.any_instance.stub(:github_access_ready).and_return(true)
+      cmd.stub(:send).with('foo')
+      cmd.stub(:respond_to?).with('foo').and_return(true)
+      gh.should_receive(:update)
+      gr.new(%w(foo))
+    end
+
+    it 'calls the command' do
       gr.any_instance.stub(:local_repo_ready).and_return(true)
       gr.any_instance.stub(:github_access_ready).and_return(true)
       gh.stub(:update)
@@ -35,25 +44,27 @@ describe 'GitReview' do
       gr.new(%w(foo))
     end
 
-    it 'does not call invalid command' do
+  end
+
+  context 'when command is invalid' do
+
+    it 'notifies the user' do
+      cmd.stub(:respond_to?).with('foo').and_return(false)
+      cmd.stub(:help)
+      gr.any_instance.should_receive(:puts).with(/'foo' is not a valid command/)
+      gr.new(%w(foo))
+    end
+
+    it 'does not call that command' do
       cmd.stub(:respond_to?).with('foo').and_return(false)
       cmd.stub(:help)
       cmd.should_not_receive('foo')
       gr.new(%w(foo))
     end
 
-    it 'calls help if invalid' do
+    it 'shows help page' do
       cmd.stub(:respond_to?).with('foo').and_return(false)
       cmd.should_receive(:help)
-      gr.new(%w(foo))
-    end
-
-    it 'calls update first unless command is clean' do
-      gr.any_instance.stub(:local_repo_ready).and_return(true)
-      gr.any_instance.stub(:github_access_ready).and_return(true)
-      cmd.stub(:send).with('foo')
-      cmd.stub(:respond_to?).with('foo').and_return(true)
-      gh.should_receive(:update)
       gr.new(%w(foo))
     end
 
