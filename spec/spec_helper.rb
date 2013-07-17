ea21ceba6d8d06d1113264e7b707a172ea9a2f95
KalabiYau <skullzeek@gmail.com>
2013-07-17 22:11:45 +0200

    Move all rspec related settings regards style to project .rspec file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,6 @@ require 'support/request_context'
 require 'support/private_context'
 
 RSpec.configure do |config|
-  # Standard values for a nice output.
-  config.color_enabled = true
   # Silence console output for all specs.
   config.before { assume_silence }
 end
