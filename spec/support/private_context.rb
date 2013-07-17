6bd9f8e7dc87d7b5db1c8f325bc40149b609cf78
KalabiYau <skullzeek@gmail.com>
2013-07-17 21:46:42 +0200

    Workaround for ruby 2.0

--- spec/support/private_context.rb
+++ spec/support/private_context.rb
@@ -3,8 +3,9 @@ unless RSpec.world.shared_example_groups[:private]
   shared_context :private do
 
     # Allow to access private methods directly to test them independently.
-    GitReview.define_method :method_missing do |name, *arguments|
-      send name, *arguments
+    class GitReview
+      meths = new.private_methods - Object.new.private_methods
+      public *meths.collect(&:to_sym)
     end
 
   end
