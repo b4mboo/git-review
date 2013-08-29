9b6c59520f3d0a8ffa4c028b2296b02ee186dc95
xystushi <xystushi@gmail.com>
2013-08-29 13:24:27 +0800

    Use GLI to parse command line options.

--- spec/git-review/commands_spec.rb
+++ spec/git-review/commands_spec.rb
@@ -13,15 +13,6 @@ describe 'Commands' do
     github.stub(:configure_github_access).and_return('username')
   end
 
-  describe '#help' do
-
-    it 'shows the help page' do
-      subject.should_receive(:puts).with(/Usage: git review <command>/)
-      subject.help
-    end
-
-  end
-
   describe '#list' do
 
     before(:each) do
@@ -40,7 +31,6 @@ describe 'Commands' do
       end
 
       it 'shows them' do
-        subject.stub(:next_arg)
         subject.should_receive(:puts).with(/Pending requests for 'some_source'/)
         subject.should_not_receive(:puts).with(/No pending requests/)
         subject.should_receive(:print_request).with(req1).ordered
@@ -49,11 +39,10 @@ describe 'Commands' do
       end
 
       it 'sorts the output with --reverse option' do
-        subject.stub(:next_arg).and_return('--reverse')
         subject.stub(:puts)
         subject.should_receive(:print_request).with(req2).ordered
         subject.should_receive(:print_request).with(req1).ordered
-        subject.list
+        subject.list(true)
       end
 
     end
@@ -66,7 +55,6 @@ describe 'Commands' do
       end
 
       it 'does not list them' do
-        subject.stub(:next_arg)
         subject.should_receive(:puts).
             with(/No pending requests for 'some_source'/)
         subject.should_not_receive(:print_request)
@@ -77,7 +65,6 @@ describe 'Commands' do
 
     it 'knows when there are no open pull requests' do
       github.stub(:current_requests_full).and_return([])
-      subject.stub(:next_arg)
       subject.should_receive(:puts).
           with(/No pending requests for 'some_source'/)
       subject.should_not_receive(:print_request)
@@ -88,40 +75,33 @@ describe 'Commands' do
 
   describe '#show' do
 
-    it 'requires an ID' do
-      subject.stub(:next_arg).and_return(nil)
-      expect { subject.show }.to raise_error(::GitReview::InvalidRequestIDError)
-    end
-
     it 'requires a valid request number' do
-      subject.stub(:next_arg).and_return(0)
       github.stub(:request_exists?).and_return(false)
-      expect { subject.show }.to raise_error(::GitReview::InvalidRequestIDError)
+      expect { subject.show(0) }.
+          to raise_error(::GitReview::InvalidRequestIDError)
     end
 
     context 'when the pull request number is valid' do
 
       before(:each) do
-        subject.stub(:get_request_or_return).and_return(request)
+        subject.stub(:get_request_by_number).and_return(request)
         subject.stub(:puts)
       end
 
       it 'shows stats of the request' do
-        subject.stub(:next_arg).and_return(nil)
         subject.should_receive(:git_call).
             with("diff --color=always --stat HEAD...#{head_sha}")
         subject.stub(:print_request_details)
         subject.stub(:print_request_discussions)
-        subject.show
+        subject.show(1)
       end
 
       it 'shows full diff with --full option' do
-        subject.stub(:next_arg).and_return('--full')
         subject.should_receive(:git_call).
             with("diff --color=always HEAD...#{head_sha}")
         subject.stub(:print_request_details)
         subject.stub(:print_request_discussions)
-        subject.show
+        subject.show(1, true)
       end
 
     end
@@ -130,146 +110,97 @@ describe 'Commands' do
 
   describe '#browse' do
 
-    it 'requires an valid ID' do
-      subject.stub(:next_arg).and_return(nil)
-      expect { subject.browse }.
-          to raise_error(::GitReview::InvalidRequestIDError)
-    end
-
     it 'opens the pull request page on GitHub in a browser' do
-      subject.stub(:get_request_or_return).and_return(request)
+      subject.stub(:get_request_by_number).and_return(request)
       Launchy.should_receive(:open).with(html_url)
-      subject.browse
+      subject.browse(1)
     end
 
   end
 
   describe '#checkout' do
 
-    it 'requires an valid ID' do
-      subject.stub(:next_arg).and_return(nil)
-      expect { subject.checkout }.
-          to raise_error(::GitReview::InvalidRequestIDError)
+    before(:each) do
+      subject.stub(:get_request_by_number).and_return(request)
+      subject.stub(:puts)
     end
 
-    context 'when the request is valid' do
+    it 'creates a headless state in the local repo with the requests code' do
+      subject.should_receive(:git_call).with("checkout pr/#{request_number}")
+      subject.checkout(1)
+    end
 
-      before(:each) do
-        subject.stub(:get_request_or_return).and_return(request)
-      end
-
-      it 'creates a headless state in the local repo with the requests code' do
-        subject.stub(:next_arg)
-        subject.should_receive(:git_call).with("checkout pr/#{request_number}")
-        subject.checkout
-      end
-
-      it 'creates a local branch if the optional param --branch is appended' do
-        subject.stub(:next_arg).and_return('--branch')
-        subject.should_receive(:git_call).with("checkout #{head_ref}")
-        subject.checkout
-      end
-
+    it 'creates a local branch if the optional param --branch is appended' do
+      subject.should_receive(:git_call).with("checkout #{head_ref}")
+      subject.checkout(1, true)
     end
 
   end
 
   describe '#approve' do
 
-    it 'requires an valid ID' do
-      subject.stub(:next_arg).and_return(nil)
-      expect { subject.approve }.
-          to raise_error(::GitReview::InvalidRequestIDError)
+    before(:each) do
+      subject.stub(:get_request_by_number).and_return(request)
+      github.stub(:source_repo).and_return('some_source')
     end
 
-    context 'when the request is valid' do
+    it 'posts an approving comment in your name to the requests page' do
+      comment = 'Reviewed and approved.'
+      github.should_receive(:add_comment).
+        with('some_source', request_number, 'Reviewed and approved.').
+        and_return(:body => comment)
+      subject.should_receive(:puts).with(/Successfully approved request./)
+      subject.approve(1)
+    end
 
-      before(:each) do
-        subject.stub(:get_request_or_return).and_return(request)
-        github.stub(:source_repo).and_return('some_source')
-      end
-
-      it 'posts an approving comment in your name to the requests page' do
-        comment = 'Reviewed and approved.'
-        github.should_receive(:add_comment).
-          with('some_source', request_number, 'Reviewed and approved.').
-          and_return(:body => comment)
-        subject.should_receive(:puts).with(/Successfully approved request./)
-        subject.approve
-      end
-
-      it 'outputs any errors that might occur when trying to post a comment' do
-        message = 'fail'
-        github.should_receive(:add_comment).
-          with('some_source', request_number, 'Reviewed and approved.').
-          and_return(:body => nil, :message => message)
-        subject.should_receive(:puts).with(message)
-        subject.approve
-      end
-
+    it 'outputs any errors that might occur when trying to post a comment' do
+      message = 'fail'
+      github.should_receive(:add_comment).
+        with('some_source', request_number, 'Reviewed and approved.').
+        and_return(:body => nil, :message => message)
+      subject.should_receive(:puts).with(message)
+      subject.approve(1)
     end
 
   end
 
   describe '#merge' do
 
-    it 'requires an valid ID' do
-      subject.stub(:next_arg).and_return(nil)
-      expect { subject.merge }.
-          to raise_error(::GitReview::InvalidRequestIDError)
+    before(:each) do
+      subject.stub(:get_request_by_number).and_return(request)
+      github.stub(:source_repo)
     end
 
-    context 'when the request is valid' do
+    it 'does not proceed if source repo no longer exists' do
+      request.head.stub(:repo).and_return(nil)
+      subject.should_receive(:print_repo_deleted)
+      subject.should_not_receive(:git_call)
+      subject.merge(1)
+    end
 
-      before(:each) do
-        subject.stub(:get_request_or_return).and_return(request)
-        subject.stub(:next_arg)
-        github.stub(:source_repo)
-      end
-
-      it 'does not proceed if source repo no longer exists' do
-        request.head.stub(:repo).and_return(nil)
-        subject.should_receive(:print_repo_deleted)
-        subject.should_not_receive(:git_call)
-        subject.merge
-      end
-
-      it 'merges the request with your current branch' do
-        msg = "Accept request ##{request_number} " +
-            "and merge changes into \"/master\""
-        subject.should_receive(:git_call).with("merge  -m '#{msg}' #{head_sha}")
-        subject.stub(:puts)
-        subject.merge
-      end
-
+    it 'merges the request with your current branch' do
+      msg = "Accept request ##{request_number} " +
+          "and merge changes into \"/master\""
+      subject.should_receive(:git_call).with("merge -m '#{msg}' #{head_sha}")
+      subject.stub(:puts)
+      subject.merge(1)
     end
 
   end
 
   describe '#close' do
 
-    it 'requires an valid ID' do
-      subject.stub(:next_arg).and_return(nil)
-      expect { subject.close }.
-          to raise_error(::GitReview::InvalidRequestIDError)
+    before(:each) do
+      subject.stub(:get_request_by_number).and_return(request)
+      github.stub(:source_repo).and_return('some_source')
     end
 
-    context 'when the request is valid' do
-
-      before(:each) do
-        subject.stub(:get_request_or_return).and_return(request)
-        subject.stub(:next_arg)
-        github.stub(:source_repo).and_return('some_source')
-      end
-
-      it 'closes the request' do
-        github.should_receive(:close_issue).with('some_source', request_number)
-        github.should_receive(:request_exists?).
-            with('open', request_number).and_return(false)
-        subject.should_receive(:puts).with(/Successfully closed request./)
-        subject.close
-      end
-
+    it 'closes the request' do
+      github.should_receive(:close_issue).with('some_source', request_number)
+      github.should_receive(:request_exists?).
+          with('open', request_number).and_return(false)
+      subject.should_receive(:puts).with(/Successfully closed request./)
+      subject.close(1)
     end
 
   end
@@ -286,27 +217,24 @@ describe 'Commands' do
       end
 
       it 'creates a local branch with review prefix' do
-        subject.stub(:next_arg).and_return(feature_name)
         subject.should_receive(:git_call).with("checkout -b #{branch_name}")
-        subject.prepare
+        subject.prepare(true, feature_name)
       end
 
       it 'lets the user choose a name for the branch' do
-        subject.stub(:next_arg).and_return(nil)
         subject.should_receive(:gets).and_return(feature_name)
         subject.should_receive(:git_call).with("checkout -b #{branch_name}")
         subject.prepare
       end
 
       it 'creates a local branch when TARGET_BRANCH is defined' do
-        subject.stub(:next_arg).and_return(feature_name)
         ENV.stub(:[]).with('TARGET_BRANCH').and_return(custom_target_name)
         subject.should_receive(:git_call).with("checkout -b #{branch_name}")
-        subject.prepare
+        subject.prepare(true, feature_name)
       end
 
       it 'sanitizes provided branch names' do
-        subject.stub(:next_arg).and_return('wild stuff?')
+        subject.stub(:gets).and_return('wild stuff?')
         subject.should_receive(:git_call).
             with(/checkout -b review_\d+_wild_stuff/)
         subject.prepare
@@ -334,7 +262,6 @@ describe 'Commands' do
     context 'when sending pull request to current repo' do
 
       before(:each) do
-        subject.stub(:next_arg).and_return(nil)
         local.stub(:source_branch).and_return(branch_name)
         local.stub(:target_branch).and_return('master')
       end
@@ -381,19 +308,17 @@ describe 'Commands' do
 
     context 'when sending pull request to upstream repo' do
 
-
-
       let(:upstream) {
         Hashie::Mash.new(:parent => {:full_name => 'upstream'})
       }
 
       before(:each) do
-        subject.stub(:next_arg).and_return('--upstream')
         github.github.stub(:login).and_return('user')
         local.stub(:source_branch).and_return(branch_name)
         local.stub(:target_branch).and_return('master')
         github.stub(:repository).and_return(upstream)
       end
+
     end
 
   end
@@ -435,31 +360,19 @@ describe 'Commands' do
       allow_message_expectations_on_nil
     end
 
-    it 'requires either an ID or the additional parameter --all' do
-      subject.args.stub(:size).and_return(0)
-      expect { subject.clean }.
-          to raise_error(::GitReview::InvalidArgumentError)
-    end
-
     it 'removes a single obsolete branch with review prefix' do
-      subject.args.stub(:size).and_return(1)
-      subject.stub(:next_arg).and_return(request_number)
-      local.should_receive(:clean_single).with(request_number)
-      subject.clean
+      local.should_receive(:clean_single).with(request_number, false)
+      subject.clean(request_number)
     end
 
     it 'removes all obsolete branches with review prefix' do
-      subject.args.stub(:size).and_return(1)
-      subject.stub(:next_arg).and_return('--all')
       local.should_receive(:clean_all)
-      subject.clean
+      subject.clean(nil, false, true)
     end
 
     it 'deletes a branch with unmerged changes with --force option' do
-      subject.args.stub(:size).and_return(2)
-      subject.stub(:next_arg).and_return(request_number, '--force')
-      local.should_receive(:clean_single).with(request_number, force=true)
-      subject.clean
+      local.should_receive(:clean_single).with(request_number, true)
+      subject.clean(request_number, true)
     end
 
   end
