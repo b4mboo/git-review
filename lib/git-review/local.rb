3db0dad1f052bedd0c67a3fd62c1d2a934dcadc8
Dominik Bamberger <bamboo@suse.com>
2013-09-02 11:09:28 +0200

    Use github.login instead of user.name for head
    Fixes #67

--- lib/git-review/local.rb
+++ lib/git-review/local.rb
@@ -182,7 +182,7 @@ module GitReview
     # @return [String] the head string used for pull requests
     def head
       # in the form of 'user:branch'
-      "#{@config['user.name']}:#{source_branch}"
+      "#{@config['github.login']}:#{source_branch}"
     end
 
     # @return [Boolean] whether already on a feature branch
