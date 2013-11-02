04f669f57159328c0ca2e86c11590da64ef10e00
Dominik Bamberger <bamboo@suse.com>
2013-11-02 12:13:50 +0100

    move methods from commands to helpers

--- lib/git-review/helpers.rb
+++ lib/git-review/helpers.rb
@@ -4,6 +4,49 @@ module GitReview
 
     private
 
+    # ask for branch name if not provided
+    # @return [String] sanitized branch name
+    def get_branch_name
+      puts 'Please provide a name for the branch:'
+      local.sanitize_branch_name gets.chomp
+    end
+
+    # @return [String] the complete feature branch name
+    def create_feature_name(new_branch)
+      "review_#{Time.now.strftime("%y%m%d")}_#{new_branch}"
+    end
+
+    # Move uncommitted changes from original_branch to a feature_branch.
+    # @return [String] the new local branch uncommitted changes are moved to
+    def move_local_changes(original_branch, feature_name)
+      feature_branch = create_feature_name(feature_name)
+      # By checking out the feature branch, the commits on the original branch
+      # are copied over. That way we only need to remove pending (local) commits
+      # from the original branch.
+      git_call "checkout -b #{feature_branch}"
+      if local.source_branch == feature_branch
+        # Save any uncommitted changes, to be able to reapply them later.
+        save_uncommitted_changes = local.uncommitted_changes?
+        git_call('stash') if save_uncommitted_changes
+        # Go back to original branch and get rid of pending (local) commits.
+        git_call("checkout #{original_branch}")
+        remote = local.remote_for_branch(original_branch)
+        remote += '/' if remote
+        git_call("reset --hard #{remote}#{original_branch}")
+        git_call("checkout #{feature_branch}")
+        git_call('stash pop') if save_uncommitted_changes
+        feature_branch
+      end
+    end
+
+    def server
+      @server ||= ::GitReview::Server.instance
+    end
+
+    def local
+      @local ||= ::GitReview::Local.instance
+    end
+
     # System call to 'git'
     def git_call(command, verbose = debug_mode, enforce_success = false)
       if verbose
